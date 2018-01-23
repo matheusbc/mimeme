7d56549781c2f1bdbaaed331203447a6c3e17993
Matheus B Campos <matheusbc@Matheuss-MacBook-Pro.local>
2018-01-23 02:21:29 -0300

    Fixed code review issues.

---
+++ MemeMe/MemeMe/Models/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Matheus B Campos on 23/01/18.
+//  Copyright © 2018 Matheus B Campos. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
--- MemeMe/MemeMe/ViewControllers/MainViewController.swift
+++ MemeMe/MemeMe/ViewControllers/MainViewController.swift
@@ -11,29 +11,35 @@ import AVFoundation
 import Photos
 
 class MainViewController: UIViewController, UINavigationControllerDelegate, Permissible {
-
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
     @IBOutlet weak var toolbar: UIToolbar!
-
-    var keyboardHeight: CGFloat = 0
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.configure(textField: topText)
+        self.configure(textField: bottomText)
+        self.configureCamera()
+    }
+
+    private func configure(textField: UITextField) {
         let memeTextAttributes:[String:Any] = [
             NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
             NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSAttributedStringKey.strokeWidth.rawValue: 1]
-        topText.defaultTextAttributes = memeTextAttributes
-        bottomText.defaultTextAttributes = memeTextAttributes
-        topText.textAlignment = .center
-        bottomText.textAlignment = .center
-        topText.textColor = UIColor.white
-        bottomText.textColor = UIColor.white
-        topText.delegate = self
-        bottomText.delegate = self
+            NSAttributedStringKey.strokeWidth.rawValue: -3.0]
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.textAlignment = .center
+        textField.textColor = UIColor.white
+        textField.delegate = self
+    }
+
+    private func configureCamera() {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            self.cameraButton.isEnabled = false
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,29 +54,24 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, Perm
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        unsubscribeFromKeyboardNotifications()
+        NotificationCenter.default.removeObserver(self)
     }
 
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
     }
-    
-    func unsubscribeFromKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
-        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
-    }
 
     @objc func keyboardWillShow(_ notification: Notification) {
         if bottomText.isFirstResponder {
-            self.keyboardHeight = getKeyboardHeight(notification)
-            view.frame.origin.y -= self.keyboardHeight
+            let keyboardHeight = getKeyboardHeight(notification)
+            view.frame.origin.y = -keyboardHeight
         }
     }
 
     @objc func keyboardWillHide(_ notification: Notification) {
         if self.view.frame.origin.y != 0 {
-            view.frame.origin.y += self.keyboardHeight
+            view.frame.origin.y = 0
         }
     }
     
@@ -80,6 +81,12 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, Perm
         return keyboardSize.cgRectValue.height
     }
 
+    func presentImagePickerWith(sourceType: UIImagePickerControllerSourceType) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = sourceType
+        self.present(pickerController, animated: true, completion: nil)
+    }
 
     // MARK: - Actions
     @IBAction func share(_ sender: Any) {
@@ -93,6 +100,13 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, Perm
         
         // exclude some activity types from the list (optional)
         activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+
+        activityViewController.completionWithItemsHandler = { (activityType: UIActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) -> Void in
+            if completed {
+                // Save image
+                _ = Meme(topText: self.topText.text!, bottomText: self.bottomText.text!, originalImage: self.imageView.image!, memedImage: image)
+            }
+        }
         
         // present the view controller
         self.present(activityViewController, animated: true, completion: nil)
@@ -108,10 +122,7 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, Perm
         cameraPermission(viewController: self) { (authorizationStatus: AVAuthorizationStatus) in
             switch authorizationStatus {
             case .authorized:
-                let pickerController = UIImagePickerController()
-                pickerController.delegate = self
-                pickerController.sourceType = .camera
-                self.present(pickerController, animated: true, completion: nil)
+                self.presentImagePickerWith(sourceType: .camera)
             default:
                 print("MimeMe hasn`t camera permission.")
             }
@@ -122,30 +133,29 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, Perm
         photosPermission(viewController: self, completion: { (authorizationStatus: PHAuthorizationStatus) in
             switch authorizationStatus {
             case .authorized:
-                let pickerController = UIImagePickerController()
-                pickerController.delegate = self
-                pickerController.sourceType = .photoLibrary
-                self.present(pickerController, animated: true, completion: nil)
+                self.presentImagePickerWith(sourceType: .photoLibrary)
             default:
                 print("MimeMe hasn`t photos permission.")
             }
         })
     }
 
+    func hideTopAndBottomBars(_ hide: Bool) {
+        self.navigationController?.setNavigationBarHidden(hide, animated: false)
+        self.toolbar.isHidden = hide
+    }
+
     func generateMemedImage() -> UIImage {
-        
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
-        self.toolbar.isHidden = true
-        
+        hideTopAndBottomBars(true)
+
         // Render view to an image
         UIGraphicsBeginImageContext(self.view.frame.size)
         view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
         let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
-        
-        self.navigationController?.setNavigationBarHidden(false, animated: false)
-        self.toolbar.isHidden = false
-        
+
+        hideTopAndBottomBars(false)
+
         return memedImage
     }
 }
@@ -153,7 +163,7 @@ class MainViewController: UIViewController, UINavigationControllerDelegate, Perm
 extension MainViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            self.imageView.contentMode = .scaleAspectFill
+            self.imageView.contentMode = .scaleAspectFit
             self.imageView.image = image
         }
         dismiss(animated:true, completion: nil)
